5284ad021103db0ced574ca705d2b09a95923fae
papagenoo <bmax@nm.ru>
2013-04-21 05:19:24 +0400

    first commit

---
+++ kern-build/CMakeFiles/kern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/kern.dir/main.c.o"
+  "kern.pdb"
+  "kern"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/kern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
